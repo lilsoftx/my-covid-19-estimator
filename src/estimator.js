44a14ccfe6c9848a5615cb5e37248cbb19934396
lilsoftx <lilsoftx@gmail.com>
2020-04-14 18:16:15 +0100

    commit 8

--- src/estimator.js
+++ src/estimator.js
@@ -12,79 +12,74 @@ const covid19ImpactEstimator = (data) => {
     population: 66622705,
     totalHospitalBeds: 1380614
   };
+  
+  let getFactor;
 
   // getting the time to elapse in days
-  function tEF() {
-    if (input.periodType === 'days') {
-      return input.timeToElapse;
-    }
-    if (input.periodType === 'weeks') {
-      return data.timeToElapse * 7;
-    }
-    if (input.periodType === 'months') {
-      const averageOfmonthsInAYear = 30;
-      return Math.round(data.timeToElapse * averageOfmonthsInAYear);
-    }
-    return true;
+  switch (input.periodType.trim().toLowerCase()) {
+    case 'months':
+      getFactor = Math.trunc((input.timeToElapse * 30) / 3);
+      break;
+    case 'weeks':
+      getFactor = Math.trunc((input.timeToElapse * 7) / 3);
+      break;
+    case 'days':
+      getFactor = Math.trunc((input.timeToElapse) / 3);
+      break;
+      default:
   }
 
-  // getting factor
-  const getFactor = () => {
-    const daysPerDoubleIncrease = 3;
-    return tEF() / daysPerDoubleIncrease;
-  };
-
   // currently Infected cases for impact and severe impact
-  const currentlyInfected = input.reportedCases * 10;
-  const severeCurrentlyInfected = input.reportedCases * 50;
+  const currentlyInfected = Math.trunc(input.reportedCases * 10);
+  const severeCurrentlyInfected = Math.trunc(input.reportedCases * 50);
 
   // infections by requested time for impact and severe impact
-  const infectionsByRequestedTime = currentlyInfected * (2 ** getFactor(input));
-  const severeInfectionsByRequestedTime = severeCurrentlyInfected * (2 ** getFactor(input));
+  const infectionsByRequestedTime = Math.trunc(currentlyInfected * (2 ** getFactor));
+  const severeInfectionsByRequestedTime = Math.trunc(severeCurrentlyInfected * (2 ** getFactor));
 
   // severe cases by requested time for impact and severe impact
-  const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
-  const severeSevereCasesByRequestedTime = severeInfectionsByRequestedTime * 0.15;
+  const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.15);
+  const severeSevereCasesByRequestedTime = Math.trunc(severeInfectionsByRequestedTime * 0.15);
 
   // hospital beds by requested time for impact and severe impact
-  const AvailableBeds = input.totalHospitalBeds * 0.35;
-  const hospitalBedsByRequestedTime = AvailableBeds - severeCasesByRequestedTime;
-  const severeHospitalBedsByRequestedTime = AvailableBeds - severeSevereCasesByRequestedTime;
+  const AvailableBeds = Math.trunc(input.totalHospitalBeds * 0.35);
+  const hospitalBedsByRequestedTime = Math.trunc(AvailableBeds - severeCasesByRequestedTime);
+  const severeHospitalBedsByRequestedTime = Math.trunc(AvailableBeds - severeSevereCasesByRequestedTime);
 
   // cases of ICU for impact and severe impact
-  const casesForICUByRequestedTime = infectionsByRequestedTime * 0.5;
-  const severeCasesForICUByRequestedTime = severeInfectionsByRequestedTime * 0.5;
+  const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
+  const severeCasesForICUByRequestedTime = Math.trunc(severeInfectionsByRequestedTime * 0.05);
 
   // cases that requires ventilation
-  const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.2;
-  const severeCasesForVentilatorsByRequestedTime = severeInfectionsByRequestedTime * 0.2;
+  const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
+  const severeCasesForVentilatorsByRequestedTime = Math.trunc(severeInfectionsByRequestedTime * 0.02);
 
   // money to be lost by economy
-  const dollarInFlight = infectionsByRequestedTime * input.region.avgDailyIncomeInUSD * tEF(input);
+  const dollarInFlight = Math.trunc(infectionsByRequestedTime * input.region.avgDailyIncomeInUSD * timeToElapse);
   const cal = severeInfectionsByRequestedTime * input.region.avgDailyIncomeInUSD;
-  const severeDollarInFlight = cal * tEF(input);
+  const severeDollarInFlight = Math.trunc(cal * tEF(input));
 
   return {
     data: input,
     impact: {
       currentlyInfected,
-      infectionsByRequestedTime: infectionsByRequestedTime.toString().split('.')[0],
-      severeCasesByRequestedTime: severeCasesByRequestedTime.toString().split('.')[0],
-      hospitalBedsByRequestedTime: hospitalBedsByRequestedTime.toString().split('.')[0],
-      casesForICUByRequestedTime: casesForICUByRequestedTime.toString().split('.')[0],
-      casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime.toString().split('.')[0],
-      dollarInFlight: dollarInFlight.toFixed(2)
+      infectionsByRequestedTime: infectionsByRequestedTime,
+      severeCasesByRequestedTime: severeCasesByRequestedTime,
+      hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
+      casesForICUByRequestedTime: casesForICUByRequestedTime,
+      casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime,
+      dollarInFlight: dollarInFlight
     },
     severeImpact: {
       currentlyInfected,
-      infectionsByRequestedTime: severeInfectionsByRequestedTime.toString().split('.')[0],
-      severeCasesByRequestedTime: severeSevereCasesByRequestedTime.toString().split('.')[0],
-      hospitalBedsByRequestedTime: severeHospitalBedsByRequestedTime.toString().split('.')[0],
-      casesForICUByRequestedTime: severeCasesForICUByRequestedTime.toString().split('.')[0],
-      casesForVentilatorsByRequestedTime: severeCasesForVentilatorsByRequestedTime.toString().split('.')[0],
-      dollarInFlight: severeDollarInFlight.toFixed(2)
+      infectionsByRequestedTime: severeInfectionsByRequestedTime,
+      severeCasesByRequestedTime: severeSevereCasesByRequestedTime,
+      hospitalBedsByRequestedTime: severeHospitalBedsByRequestedTime,
+      casesForICUByRequestedTime: severeCasesForICUByRequestedTime,
+      casesForVentilatorsByRequestedTime: severeCasesForVentilatorsByRequestedTime,
+      dollarInFlight: severeDollarInFlight
     }
   };
 };
-
-export default covid19ImpactEstimator;
+console.log(covid19ImpactEstimator())
+//export default covid19ImpactEstimator;
